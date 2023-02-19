5233f816de1b53a061cbd44c61af31d7ca0ff774
Egidijus <egidijusstalmokas96@gmail.com>
2023-02-19 15:56:59 +0200

    Added logo to navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Info;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer(['admin.layouts.left_menu', 'auth.login', 'auth.passwords.email', 'auth.passwords.reset'], function ($view) {
+            $logo = Info::select('type', 'value')->where('type', 'logo')->first();
+            $view->with(['logo' => $logo ]);
+        });
     }
 }
--- resources/views/admin/layouts/left_menu.blade.php
+++ resources/views/admin/layouts/left_menu.blade.php
@@ -1,10 +1,10 @@
     <!-- Sidebar -->
     <div style="overflow:scroll;" class="sidebar-fixed position-fixed">
       <a class="logo-wrapper waves-effect">
-        <img style="width: 100px; margin-left: 15%;" src="" class="img-fluid" alt="">
+        <img style="width: 100px; margin-left: 15%;" src="{{ asset('logo/') }}/{{ $logo->value }}" class="img-fluid" alt="">
       </a> 
       <div class="list-group list-group-flush">
-        <a href="/desk" class="list-group-item waves-effect">
+        <a href="{{ route('desk') }}" class="list-group-item waves-effect">
           <i class="fa fa-pie-char mr-3"></i>Dashboard</a>
         <a href="{{ route('question.index') }}" class="list-group-item list-group-item-action waves-effect">
           <i class="fa fa-money mr-3"></i>Question</a> 
