9ea1c3049010fb0da3f6573dc86b0959fe62f0d6
Egidijus <egidijusstalmokas96@gmail.com>
2023-02-19 16:19:28 +0200

    Active menu buttons

--- resources/views/admin/layouts/left_menu.blade.php
+++ resources/views/admin/layouts/left_menu.blade.php
@@ -4,13 +4,12 @@
         <img style="width: 100px; margin-left: 15%;" src="{{ asset('logo/') }}/{{ $logo->value }}" class="img-fluid" alt="">
       </a> 
       <div class="list-group list-group-flush">
-        <a href="{{ route('desk') }}" class="list-group-item waves-effect">
-          <i class="fa fa-pie-char mr-3"></i>Dashboard</a>
-        <a href="{{ route('question.index') }}" class="list-group-item list-group-item-action waves-effect">
-          <i class="fa fa-money mr-3"></i>Question</a> 
-        <a href="{{ route('settings.main') }}" class="list-group-item list-group-item-action waves-effect">
-          <i class="fa fa-money mr-3"></i>Settings</a> 
+        <a href="{{ route('desk') }}" class="list-group-item {{ (request()->segment(2) == '') ? 'active' : '' }} waves-effect">
+          <i class="mr-3"></i>Dashboard</a>
+        <a href="{{ route('question.index') }}" class="list-group-item list-group-item-action {{ (request()->segment(2) == 'question') ? 'active' : '' }} waves-effect">
+          <i class="fa-solid fa-question mr-3"></i>Question</a> 
+        <a href="{{ route('settings.main') }}" class="list-group-item list-group-item-action {{ (request()->segment(2) == 'settings') ? 'active' : '' }} waves-effect">
+          <i class="fa-solid fa-gear mr-3"></i>Settings</a> 
       </div>
-
     </div>
     <!-- Sidebar -->
